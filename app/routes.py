88e281c86738509782186d0b238108997ffcd837
Angelica <angelica.yee57@gmail.com>
2023-04-20 23:47:33 -0500

    new endpoint /broken

--- app/routes.py
+++ app/routes.py
@@ -16,3 +16,13 @@ def say_hello_json():
         "Hobbies":["netflix","chess"]
     }
 
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"] + new_hobby
+    return response_body
